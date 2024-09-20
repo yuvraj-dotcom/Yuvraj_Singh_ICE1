2433db5723cc5431a345b0fd2ff372dc549f3f71
Yuvraj <officialyuvrajs@gmail.com>
2024-09-20 15:21:07 -0400

    Imported scanner and completed user input task

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -5,6 +5,7 @@
 package card;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -29,6 +30,23 @@ public class CardTrick {
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a card value (1-13): ");
+        int userValue = scanner.nextInt();
+        scanner.nextLine();
+        if (userValue < 1 || userValue > 13){
+            System.out.println("Invalid input!! Please enter a value between 1 and 13");
+            return;
+        }
+        
+        System.out.print("Enter a suit (0-3 where 0=Hearts, 1=Diamonds, 2=Spades, 3=Clubs): ");
+        int userSuit = scanner.nextInt();
+        scanner.nextLine();
+        
+        if (userSuit < 0 || userSuit > 3){
+            System.out.println("Invalid input!! Please enter a value between 0 and 3");
+            return;
+        }
         // and search magicHand here
         //Then report the result here
         // add one luckcard hard code 2,clubs
